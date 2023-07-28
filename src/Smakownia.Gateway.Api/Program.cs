d518a67c3255564cb7bacc8b228f39315e71c749
aruzo <dawid12391111@gmail.com>
2023-07-28 18:18:41 +0200

    add basket api

--- src/Smakownia.Gateway.Api/Program.cs
+++ src/Smakownia.Gateway.Api/Program.cs
@@ -1,4 +1,23 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var uiOrigin = "_uiOrigin";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+                     .AddJsonFile("ocelot.json", optional: false)
+                     .AddEnvironmentVariables();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(uiOrigin, b => b.WithOrigins(builder.Configuration.GetValue<string>("UI_ORIGIN"))
+                                      .AllowAnyMethod()
+                                      .AllowAnyHeader());
+});
+
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -9,12 +28,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(uiOrigin);
 
-app.UseAuthorization();
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = "/swagger/docs";
+});
+
+await app.UseOcelot();
 
 app.MapControllers();
 
